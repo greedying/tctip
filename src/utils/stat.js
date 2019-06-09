7ceaf008c32dec302c3960d3934a7b6e2bd51671
greedying <greedying@163.com>
2019-06-09 19:02:26 +0800

    统计代码支持https

--- src/utils/stat.js
+++ src/utils/stat.js
@@ -1,7 +1,7 @@
 import { createElement } from './dom'
 
 const version = require('../../package.json').version
-const statUrl = 'http://stat.tctip.com/stat/index'
+const statUrl = '//stat.tctip.com/stat/index'
 
 export default function () {
   let url = `${statUrl}/version/${version}`
